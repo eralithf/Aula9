be3c11d462680051ddb96f7e4eb575ec1f5c8d3d
Luiz Felipe <luipedro93@gmail.com>
2025-04-10 20:04:11 -0300

    terminei

--- app/src/main/java/com/example/aula/MainActivity.java
+++ app/src/main/java/com/example/aula/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.aula;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,17 +15,23 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     EditText editCod, editNome, editEmail, editTel;
     Button btnSalvar, btnExcluir, btnLimpar;
     ListView lista;
-
-
+    PessoasDataBase bd = new PessoasDataBase(this);
+    ArrayAdapter<String> adapter;
+    ArrayList<String> arrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Inicializar views
         editCod = findViewById(R.id.PlainCod);
         editNome = findViewById(R.id.PlainNome);
         editTel = findViewById(R.id.PlainTel);
@@ -31,6 +41,88 @@ public class MainActivity extends AppCompatActivity {
         btnExcluir = findViewById(R.id.btn3);
         lista = findViewById(R.id.lista);
 
+        // Configurar listeners
+        btnLimpar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                limparCampo();
+            }
+        });
 
+        btnSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String codigo = editCod.getText().toString();
+                String nome = editNome.getText().toString();
+                String tel = editTel.getText().toString();
+                String email = editEmail.getText().toString();
+
+                if (nome.isEmpty()) {
+                    editNome.setError("Campo Obrigatório!");
+                } else if (codigo.isEmpty()) {
+                    bd.AddPessoa(new Pessoas(nome, tel, email));
+                    Toast.makeText(MainActivity.this, "Adicionado com Sucesso!", Toast.LENGTH_SHORT).show();
+                    limparCampo();
+                    listarPessoas();
+                } else {
+                    bd.atualizaPessoa(new Pessoas(Integer.parseInt(codigo), nome, tel, email));
+                    Toast.makeText(MainActivity.this, "Atualizado com Sucesso!", Toast.LENGTH_SHORT).show();
+                    limparCampo();
+                    listarPessoas();
+                }
+            }
+        });
+
+        btnExcluir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String codigo = editCod.getText().toString();
+                if (codigo.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Não há nada selecionado!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Pessoas pessoa = new Pessoas();
+                    pessoa.setCod(Integer.parseInt(codigo));
+                    bd.ApagarPessoa(pessoa);
+                    Toast.makeText(MainActivity.this, "Apagado com Sucesso!", Toast.LENGTH_SHORT).show();
+                    limparCampo();
+                    listarPessoas();
+                }
+            }
+        });
+
+        // Configurar lista
+        listarPessoas();
+        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String conteudo = (String) lista.getItemAtPosition(position);
+                String codigo = conteudo.substring(0, conteudo.indexOf("-"));
+                Pessoas pessoa = bd.selecionarPessoa(Integer.parseInt(codigo));
+                editCod.setText(String.valueOf(pessoa.getCod()));
+                editNome.setText(pessoa.getNome());
+                editTel.setText(pessoa.getTel());
+                editEmail.setText(pessoa.getEmail());
+            }
+        });
+    }
+
+    public void listarPessoas() {
+        List<Pessoas> pessoas = bd.listarTodos();
+        arrayList = new ArrayList<String>();
+        adapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1, arrayList);
+        lista.setAdapter(adapter);
+        for (Pessoas p : pessoas) {
+            arrayList.add(p.getCod() + " - " + p.getNome());
+            adapter.notifyDataSetChanged();
+        }
+    }
+
+    public void limparCampo() {
+        editCod.setText("");
+        editNome.setText("");
+        editTel.setText("");
+        editEmail.setText("");
+        editNome.requestFocus();
     }
 }
--- app/src/main/java/com/example/aula/PessoasDataBase.java
+++ app/src/main/java/com/example/aula/PessoasDataBase.java
@@ -1,12 +1,24 @@
 package com.example.aula;
 
 import android.app.people.PeopleManager;
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import  android.database.sqlite.SQLiteDatabase;
 import  android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PessoasDataBase extends SQLiteOpenHelper {
     private static final int versao = 1;
     private static final String nomeBD = "bd_pessoas";
+
+    private static final String tb_pessoas = "pessoas";
+    private static final String c_cod = "cod";
+    private static final String c_nome = "nome";
+    private static final String c_tel = "tel";
+    private static final String c_email = "email";
 
     public PessoasDataBase(Context context) {
         super(context, nomeBD, null, versao);
@@ -18,8 +30,10 @@ public class PessoasDataBase extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
-
+        String query = "CREATE TABLE " + tb_pessoas + "("+
+                c_cod + " INTEGER PRIMARY KEY, " + c_nome + " TEXT, "+
+                c_tel + " TEXT, "+c_email +" TEXT)";
+        db.execSQL(query);
     }
 
     @Override
@@ -27,17 +41,66 @@ public class PessoasDataBase extends SQLiteOpenHelper {
 
     }
 
+public void AddPessoa (Pessoas pessoa) {
+        SQLiteDatabase db = this.getWritableDatabase();
+    ContentValues values = new ContentValues();
+    values.put(c_nome,pessoa.getNome());
+    values.put(c_tel,pessoa.getTel());
+    values.put(c_email,pessoa.getEmail());
+    db.insert(tb_pessoas, null,values);
+    db.close();
+}
 
+public void ApagarPessoa (Pessoas pessoa) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(tb_pessoas, c_cod+" = ?",new String[]{String.valueOf((pessoa.getCod()))});
+        db.close();
+}
 
+public Pessoas selecionarPessoa (int cod) {
+        SQLiteDatabase db = this.getReadableDatabase();
+    Cursor cursor = db.query(tb_pessoas,
+            new String[]{c_cod,c_nome,c_tel,c_email},
+            c_cod+" ?",
+            new String[]{String.valueOf(cod)},
+            null, null, null);
+    if(cursor!=null) cursor.moveToFirst();
+    else return null;
+    Pessoas pessoa1 = new Pessoas(Integer.parseInt(cursor.getString(0)),
+            cursor.getString(1),cursor.getString(2),
+            cursor.getString(3));
+    return pessoa1;
+}
 
+public List <Pessoas> listarTodos () {
+    SQLiteDatabase db = this.getReadableDatabase();
+    List<Pessoas> lista = new ArrayList<Pessoas>();
+    String query = "SELECT * FROM " + tb_pessoas;
+    Cursor cursor = db.rawQuery(query, null);
+    if (cursor.moveToFirst()) {
+        do {
+            Pessoas pessoa = new Pessoas();
+            pessoa.setCod(Integer.parseInt(cursor.getString(0)));
+            pessoa.setNome(cursor.getString(1));
+            pessoa.setTel(cursor.getString(2));
+            pessoa.setEmail(cursor.getString(3));
+            lista.add(pessoa);
+        }while (cursor.moveToNext());
 
+    }
+return lista;
+}
 
-
-
-
-
-
-
+public void atualizaPessoa (Pessoas pessoa) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(c_nome,pessoa.getNome());
+        values.put(c_tel,pessoa.getTel());
+        values.put(c_email,pessoa.getEmail());
+        db.update(tb_pessoas,values, c_cod + " = ?",
+                new String[]{String.valueOf(pessoa.getCod())});
+        db.close();
+}
 
 
 
